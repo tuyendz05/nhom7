3806eed1b71fe25c3d84691e1da528b89a63fea7
Nguyen Ba Tuyen <batuyen0510@gmail.com>
2024-07-31 09:03:41 +0700

    ..

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
+res.send('Hello, CI/CD with Render! This is an updated version.');
